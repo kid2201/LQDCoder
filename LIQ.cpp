85497c7b47a9234cb02bb4f650c1871fe6c6387d
kid2201 <48373691+kid2201@users.noreply.github.com>
2020-04-15 11:44:26 +0700

    Add LIQ.cpp

--- LIQ.cpp
+++ LIQ.cpp
@@ -5,10 +5,7 @@ int main(){
     cin>>n;
     for (int i = 1; i <= n ; i++) cin>>a[i];
     for (int i = 1; i <=n; i++){
-        L[i] = 1;
-        for (int j = 1; j < i; j++)
-        if (a[i] > a[j] && L[i] < L[j]+1)
-            L[i] = L[j] + 1;
+         
         res = max(L[i],res);
     }
     cout<<res;
